33f13d86a0472287f5202f58de9c3d31c55d6606
Yuragranovsky <yuragranovsky@gmail.com>
2023-09-21 13:21:00 +0300

    final prototype code

--- src/chat-appl.mjs
+++ src/chat-appl.mjs
@@ -20,7 +20,7 @@ app.ws('/contacts/websocket', (ws, req) => {
 })
 app.ws('/contacts/websocket/:clientName', (ws, req) => {
     const clientName = req.params.clientName;
-   processConnection(clientName, ws);
+    processConnection(clientName, ws);
 
 })
 app.listen(8080);
@@ -28,5 +28,37 @@ function processConnection(clientName, ws) {
     const connectionId = crypto.randomUUID();
     chatRoom.addConnection(clientName, connectionId, ws);
     ws.on('close', () => chatRoom.removeConnection(connectionId));
+    ws.on('message', processMessage.bind(undefined, clientName, ws))
+}
+function processMessage(clientName, ws, message) {
+    try {
+        const messageObj = JSON.parse(message.toString())
+        const to = messageObj.to;
+        const text = messageObj.text;
+        if (!text) {
+            ws.send("your message doesn't contain text")
+        } else {
+            const objSent = JSON.stringify({from: clientName, text});
+            if (!to || to === 'all') {
+                sendAll(objSent);
+            } else {
+                sendClient(objSent, to, ws);
+            }
+        }
+
+    } catch (error) {
+        ws.send('wrong message structure')
+    }
+}
+function sendAll(message) {
+    chatRoom.getAllWebsockets().forEach(ws => ws.send(message));
+}
+function sendClient(message, client, socketFrom) {
+    const clientSockets = chatRoom.getClientWebsockets(client);
+    if(clientSockets.length == 0) {
+        socketFrom.send(client + " contact doen't exist")
+    } else {
+        clientSockets.forEach(s => s.send(message))
+    }
 }
 
